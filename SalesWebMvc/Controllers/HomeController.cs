25e67ddc38aa6510f1bf8061e32fe92f2a5cc9ba
Pedro Rafael <pedrorafael1830@gmail.com>
2022-08-11 15:37:01 -0300

    Primeiro controller e testes nas páginas Razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "SalesWebMVC from C# Course";
+            ViewData["Student"] = "Pedro Rafael";
 
             return View();
         }
